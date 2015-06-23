0494f2ba5e879174e7ff1f3626ef8013aaad8fe1
Scott Williams <williamsjs1224@gmail.com>
2015-06-22 22:38:46 -0400

    optimize controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,15 @@
 class ReportsController < ApplicationController
   def all_data
-    @assembly = Assembly.find_by_name(params[:name])
+    @assembly = Assembly.where(name: params[:name]).take
     @hits = []
-    @assembly.sequences.each do |s|
-      s.genes.each do |g|
+    Sequence.where(assembly_id: @assembly.id).find_each do |s|
+      Gene.where(sequence_id: s.id).find_each do |g|
         g.hits.each do |h|
           @hits << h
         end
       end
     end
+
     @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
   end
 end
