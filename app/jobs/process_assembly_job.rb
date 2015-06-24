fee6accdfd756f0a2a461e19e35adf8aba42b590
Scott Williams <williamsjs1224@gmail.com>
2015-06-23 22:34:47 -0400

    clean up method in job

--- app/jobs/process_assembly_job.rb
+++ app/jobs/process_assembly_job.rb
@@ -2,21 +2,11 @@ class ProcessAssemblyJob < ActiveJob::Base
   queue_as :default
 
   def perform(assembly, email)
-    @assembly = Assembly.where(name: params[:name]).take
-    @hits = []
-    Sequence.where(assembly_id: @assembly.id).find_each do |s|
-      Gene.where(sequence_id: s.id).find_each do |g|
-        g.hits.each do |h|
-          @hits << h
-        end
-      end
-    end
-    @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
-    begin
-    AssemblyMailer.report(@email).deliver_now
-    flash.now[:notice] = "created"
-    rescue Net::SMTPAuthenticationError, Net::SMTPServerBusy, Net::SMTPSyntaxError, Net::SMTPFatalError, Net::SMTPUnknownError => e
-      flash.now[:notice] = "Problems sending mail"
-    end
+    @assembly = Assembly.where(name: assembly)
+    @hits = Hit.where(subject_id: Gene.where(
+      sequence_id: Sequence.where(
+      assembly_id: Assembly.where(name: name)))).order(:percent_similarity)
+    AssemblyMailer.report(@email, @hits, @assembly).deliver_now
+  end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module DatabaseOptimizations
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :delayed_job
     config.autoload_paths << Rails.root.join('app/jobs')
+    Delayed::Worker.destroy_failed_jobs = false
+
   end
 end
